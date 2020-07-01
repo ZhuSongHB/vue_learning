9efd59f2cc59ccc180a78af48899f43ed12fa868
song <719759485@qq.com>
2020-07-01 09:42:33 +0800

    记事本  ------   methods心得    如果函数功能一样，那么可以在传参数的时候传入一个固定的值，用来区分哪一部分（前提功能一样）

---
+++ toDoList/index.js
@@ -0,0 +1,40 @@
+const vm = new Vue({
+    el: "#app",
+    value: '',
+    data: {
+        underway: [],
+        duplicate: [],
+        alreadyCompleted: []
+    },
+    methods: {
+        add() {
+            if (this.value == undefined || this.duplicate.indexOf(this.value) >= 0) {
+                return;
+            }
+            this.duplicate.push(this.value)
+            this.underway.push({
+                title: this.value,
+                id: Math.random()
+            })
+        },
+        handleInput(e) {
+            this.value = e.target.value
+        },
+        delivery(index, type) {
+            if (type === 'need') {
+                const item = this.underway.splice(index, 1)
+                this.alreadyCompleted.push(...item)
+            } else {
+                const item = this.alreadyCompleted.splice(index, 1)
+                this.underway.push(...item)
+            }
+        },
+        delte(index, type) {
+            if (type === 'need') {
+                this.underway.splice(index, 1)
+            } else {
+                this.alreadyCompleted.splice(index, 1)
+            }
+        }
+    }
+})
